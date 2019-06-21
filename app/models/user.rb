dd686e13e71f535e8aae98f2750e65fdc97ac257
MohamedYakout <mohamed.yakout91@gmail.com>
2019-06-21 23:52:30 +0200

    Init functions to fetch h1 headers from URL

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,30 @@
+require 'nokogiri'
+
 class User < ApplicationRecord
 
   before_validation :set_shorten_url
+
+  def get_response_with_redirect(uri)
+    response = Net::HTTP.get_response(uri)
+    if response.code == "301"
+      response = Net::HTTP.get_response(URI.parse(response.header['location']))
+    end
+    response
+  end
+
+  def get_headers
+    uri = URI.parse(self.original_url)
+    # response = Net::HTTP.get_response(uri.host, "/")
+    response = get_response_with_redirect(uri)
+    body = response.body
+    nokogiri_obj = Nokogiri::HTML.fragment(body)
+    nokogiri_obj.search('h1')
+
+    # Net::HTTP.start(uri.host, uri.port) do |http|
+    #   response = http.head(uri.path)
+    #   puts response
+    # end
+  end
 
   protected
 
