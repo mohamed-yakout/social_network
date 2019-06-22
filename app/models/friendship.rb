a88118b4c77b30856d126dad21daa0f04888c297
MohamedYakout <mohamed.yakout91@gmail.com>
2019-06-22 22:09:03 +0200

    Add validates of Friendship

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,6 +2,10 @@ class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
 
+  validates :user, presence: true
+  validates :friend, presence: true, uniqueness: { scope: :user }
+  validate :friend_not_same_uer
+
   after_create :create_bi_directional_relationship
   after_destroy :destroy_bi_directional_relationship
 
@@ -13,5 +17,9 @@ class Friendship < ApplicationRecord
     def destroy_bi_directional_relationship
       friendship = friend.friendships.find_by(friend: user)
       friendship.destroy if friendship
+    end
+
+    def friend_not_same_uer
+      errors.add(:friend, "can't be equal to user") if user == friend
     end
 end
