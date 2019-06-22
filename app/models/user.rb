726b607443b8043ed3eb29045310d98ca5f1039a
MohamedYakout <mohamed.yakout91@gmail.com>
2019-06-22 13:51:34 +0200

    Add Callback function for adding Headings

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,10 @@ class User < ApplicationRecord
 
   has_many :headings
 
+  HEADING_TYEPS = %w(h1 h2 h3)
+
   before_validation :set_shorten_url
+  after_create :set_headings
 
   def get_response_with_redirect(uri)
     response = Net::HTTP.get_response(uri)
@@ -14,18 +17,29 @@ class User < ApplicationRecord
     response
   end
 
-  def get_headers
+  def get_headings html_type='h1'
     uri = URI.parse(self.original_url)
     # response = Net::HTTP.get_response(uri.host, "/")
     response = get_response_with_redirect(uri)
     body = response.body
     nokogiri_obj = Nokogiri::HTML.fragment(body)
-    nokogiri_obj.search('h1')
+    nokogiri_obj.search(html_type)
 
     # Net::HTTP.start(uri.host, uri.port) do |http|
     #   response = http.head(uri.path)
     #   puts response
     # end
+  end
+
+  def set_headings
+    return unless self.headings.blank?
+    HEADING_TYEPS.each do |heading_type|
+      nokogiri_arr = self.get_headings heading_type
+      nokogiri_arr.each do |nokogiri_element|
+        Heading.create(user_id: self.id, html_value: nokogiri_element.content,
+          html_type: heading_type, html_element: nokogiri_element.to_html)
+      end
+    end
   end
 
   protected
