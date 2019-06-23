ac2d696dc51e26490b1360613021eff2343637c8
MohamedYakout <mohamed.yakout91@gmail.com>
2019-06-23 08:47:39 +0200

    Add internal headings in User Profile

--- app/models/heading.rb
+++ app/models/heading.rb
@@ -1,3 +1,5 @@
 class Heading < ApplicationRecord
   belongs_to :user
+
+  self.per_page = 20
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   resources :friendships
   resources :headings
-  resources :users
+  resources :users do
+    member do
+      get 'headings'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'users#index'
